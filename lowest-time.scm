7f8aea3c59727a768ca766df1e42ab74be55df61
Ferdi Schmidt <ferdis@keytelematics.com>
2017-10-02 22:37:12 +0200

    fix(cond): use cond to clean up branching

--- lowest-time.scm
+++ lowest-time.scm
@@ -7,12 +7,14 @@
        (minutes (cons (list-ref time 2) (list-ref time 3)))
        (seconds (cons (list-ref time 4) (list-ref time 5))))
 
-    ; Check that hours is less than 24
-    (if (> (+ (* 10 (car hours)) (cdr hours)) 23) #f
-        ; Check that seconds is less than 60
-        (if (>= (+ (* 10 (car minutes)) (cdr minutes)) 60) #f
-            ; Checks that seconds is less than 60
-            (if (>= (+ (* 10 (car seconds)) (cdr seconds)) 60) #f #t)))))
+    (cond 
+      ; Check that hours is less than 24
+      ((> (+ (* 10 (car hours)) (cdr hours)) 23) #f)
+      ; Check that seconds is less than 60
+      ((>= (+ (* 10 (car minutes)) (cdr minutes)) 60) #f)
+      ; Checks that seconds is less than 60
+      ((>= (+ (* 10 (car seconds)) (cdr seconds)) 60) #f)
+      (else #t))))
 
 ; Given a list of
 ; '((1 2 3 4 5 6) (4 5 9 3 7 7) (0 0 0 7 8 9))
